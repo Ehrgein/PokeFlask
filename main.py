118c12c5aa1ffd4974a50dd12a082f3b9a9c5a74
Alek <ehrgein@gmail.com>
2022-08-29 19:52:50 -0300

    lots of css done, added icons, must change design

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import json
 import os
 from flask.views import MethodView
 from flask import Flask, render_template, request
-from wtforms import Form, StringField, SubmitField, validators
 from search import PokeSearch
 import mysql.connector
 from datetime import datetime
@@ -17,50 +16,58 @@ db = mysql.connector.connect(
     )
 
 
+
 app = Flask(__name__, static_folder="static")
-
-
 
 
 class HomePage(MethodView):
 
-    @app.route("/", methods=["GET","POST"])
+
+    @app.route("/", methods=["GET"])
+    def basepage():
+
+        return render_template('base.html')
+
+    @app.route("/", methods=["GET", "POST"])
     def get():
 
+            
+            poke_form = request.form["name"]
+            newpoke = poke_form
+            retrieve_search = PokeSearch(newpoke)
 
-
-        poke_form = request.form["nm"]
-        newpoke = poke_form
-        retrieve_search = PokeSearch(newpoke)
-
-        return render_template('pokedex.html',pokeform=poke_form.lower(),
-        namepokemonbystr = retrieve_search.pokemon.capitalize(),
-        namepokemonbyid=retrieve_search.pokename.capitalize(),
-        pokeid = retrieve_search.pokeid,
-        generation = retrieve_search.generation,
-        pokehp=retrieve_search.hp,
-        pokeatk = retrieve_search.attack,
-        pokedef = retrieve_search.defense,
-        pokespecialatk = retrieve_search.specialatk,
-        pokespecialdef = retrieve_search.specialdef,
-        pokespd = retrieve_search.speed,
-        poketype = retrieve_search.types.capitalize(),
-        pokestrong = retrieve_search.strong,
-        pokeweak = retrieve_search.weak,
-        pokeimmuneto = retrieve_search.nodamageto,
-        pokeimmunefrom = retrieve_search.nodamagefrom,
-        sprite = retrieve_search.sprite,
-        )
-
-    def save_pokemon():
-        print("pepe")
-
-   
+            return render_template('pokedex.html',pokeform=poke_form.lower(),
+            namepokemonbystr = retrieve_search.pokemon.capitalize(),
+            namepokemonbyid=retrieve_search.pokename.capitalize(),
+            pokeid = retrieve_search.pokeid,
+            generation = retrieve_search.generation,
+            pokehp=retrieve_search.hp,
+            pokeatk = retrieve_search.attack,
+            pokedef = retrieve_search.defense,
+            pokespecialatk = retrieve_search.specialatk,
+            pokespecialdef = retrieve_search.specialdef,
+            pokespd = retrieve_search.speed,
+            poketype = retrieve_search.types,
+            poketypetwo = retrieve_search.typestwo,
+            poketypelast = retrieve_search.typelast,
+            pokestrong = retrieve_search.strong,
+            pokeweak = retrieve_search.weak,
+            pokeimmuneto = retrieve_search.nodamageto,
+            pokeimmunefrom = retrieve_search.nodamagefrom,
+            sprite = retrieve_search.sprite,
+            pokebg = retrieve_search.bgcolour,
+            pokeborder = retrieve_search.bordercolour,
+            innerborder = retrieve_search.innerborder,
+            twotypeborder = retrieve_search.typetwoborder,
+            abilityone = retrieve_search.abilityone,
+            abilitytwo = retrieve_search.abilitytwo,
+            weakone = retrieve_search.weakone,
+            weaktwo = retrieve_search.weaktwo,
+            weakthree = retrieve_search.weakthree,
+            )
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-
-app.add_url_rule('/caught', view_func=HomePage.as_view('catch_em_all'))
 
 app.run(debug=True)
 
--- search.py
+++ search.py
@@ -1,3 +1,5 @@
+from inspect import Attribute
+from types import NoneType
 from urllib import response
 import requests
 import json
@@ -10,10 +12,11 @@ class PokeSearch:
     type_url = "https://pokeapi.co/api/v2/type/"
 
     def __init__(self, pokemon):
+        try:
             self.pokemon = pokemon.lower()
             content = self.__get_pokemon()
             self.pokeid = content['game_indices'][-1]['game_index']
-            self.pokename = content['forms'][0]['name']
+            self.pokename = content['name']
             self.generation = self.__getgeneration()
             self.hp = content['stats'][0]['base_stat']
             self.attack = content['stats'][1]['base_stat']
@@ -22,18 +25,44 @@ class PokeSearch:
             self.specialdef = content['stats'][4]['base_stat']
             self.speed = content['stats'][5]['base_stat']   
             self.types = content['types'][0]['type']['name']
-            self.sprite = content['sprites']['front_default']
+            self.typelast = content['types'][-1]['type']['name']
+            self.typestwo = self.__typetwo()
+            self.sprite = content['sprites']['other']['official-artwork']['front_default']
+            self.abilityone = content['abilities'][0]['ability']['name']
+            self.abilitytwo = content['abilities'][-1]['ability']['name']
             self.strong = self.__strong()
             self.weak = self.__weak()
             self.nodamageto = self.__nodamageto()
             self.nodamagefrom = self.__nodamagefrom()
+            self.bgcolour = self.__backgroundtype()
+            self.bordercolour = self.__bordertype()
+            self.innerborder = self.__innerborder()
+            self.typetwoborder = self.__typetwoborder()
+            self.weakone = self.__doubledamagetoone()
+            self.weaktwo = self.__doubledamagetotwo()
+            self.weakthree = self.__doubledamagetothree()
+
+
+        except AttributeError:
+            pass
+
+    def __typetwo(self):
+
+        response = requests.get(self.base_url + self.pokemon)
+        content = response.json()
+        contentlast = content['types'][-1]['type']['name']
+        
+        value = ""
+        if self.typelast == self.types:
+            return value 
+        else:
+            return contentlast
 
     def __get_pokemon(self):
 
             response = requests.get(self.base_url + self.pokemon)
             content = response.json()
             return content
-
 
     def __getgeneration(self):
 
@@ -49,7 +78,6 @@ class PokeSearch:
             return gens[3]
         elif 494 <= self.pokeid <= 649:
             return gens[4]
-
 
     def __strong(self):        
         #type_req = open("./weakstrong.json", encoding="utf-8")
@@ -93,7 +121,24 @@ class PokeSearch:
         else: 
             if nodamagefrom==[]:
                 return " - "
+    
+    def __doubledamagetoone(self):
+        response = requests.get(self.type_url + self.types)
+        datatype = response.json()
+        weakdata = datatype['damage_relations']['double_damage_to'][0]['name']
+        return weakdata
 
+    def __doubledamagetotwo(self):
+        response = requests.get(self.type_url + self.types)
+        datatype = response.json()
+        weakdata = datatype['damage_relations']['double_damage_to'][1]['name']
+        return weakdata
+    
+    def __doubledamagetothree(self):
+        response = requests.get(self.type_url + self.types)
+        datatype = response.json()
+        weakdata = datatype['damage_relations']['double_damage_to'][-1]['name']
+        return weakdata
 
     def __weak(self):
         response = requests.get(self.type_url + self.types)
@@ -106,7 +151,111 @@ class PokeSearch:
             weakclean= ' , '.join([str(item)for item in weak])
         return weakclean
 
+    def __backgroundtype(self):
+        
+        innercolour = {'normal': '#A8A878',
+        'dragon' : '#7038F8',
+        'water' : '#6890f0',
+        'fire' : '#F08030',
+        'ground' : '#B8A038',
+        'rock' : '#E0C068',
+        'electric' : '#F8D030',
+        'fairy': '#EE99AC',
+        'fighting': '#D67873',
+        'flying' : '#A8A878',
+        'ghost' : '#705898',
+        'grass' : '#A7DB8D',
+        'ice' : '#BCE6E6',
+        'poison' : '#A040A0',
+        'psychic' : '#F85888',
+        'steel' : '#B8B8D0',
+        'dark' : '#705848',
+        'bug' : '#C6D16E',
+        }
 
 
+        for key, value in innercolour.items():
+            if self.types in key:
+                return value
+
+    def __bordertype(self):
+                
+                
+        colourborders= {'normal': '#6D6D4E;',
+        'dragon': '#A890F0;',
+        'water': '#445E9C',
+        'fire': '#EB5353',
+        'ground': '#E0C068',
+        'rock': '#E0C068',
+        'electric': '#A1871F',
+        'fairy': '#9B6470',
+        'fighting': '#7D1F1A',
+        'flying': '#A890F0',
+        'ghost' : '#682A68',
+        'grass' : '#A040A0',
+        'ice' : '#A890F0',
+        'poison' : '#682A68',
+        'psychic' : '#A13959',
+        'steel' : '#E0C068',
+        'dark' : '#49392F',
+        'bug' : '#6D7815',
+        }
+
+          
+        for key, value in colourborders.items():
+            if self.types in key:
+                return value
+
+    def __innerborder(self):
+        
+        innerborders = {'normal' : '#C6C6A7',
+        'water': '#9db7f5',
+        'ice':'#98D8D8',
+        'ghost' : '#A292BC',
+        'fire' : '#F5AC78',
+        'ground' : '#D1C17D',
+        'rock' : '#D1C17D',
+        'electric' : '#FAE078',
+        'fairy' : '#F4BDC9',
+        'grass' : '#A7DB8D',
+        'dragon' : '#A27DFA',
+        'fighting' : '#D67873',
+        'flying' : '#C6C6A7',
+        'poison' : '#C183C1',
+        'psychic' : '#FA92B2',
+        'steel' : '#1D1E0',
+        'dark' : '#A29288',
+        'bug' : '#C6D16E',
+        }
+
+        for key, value in innerborders.items():
+            if self.types in key:
+                return value
+
+    def __typetwoborder(self):
+
+        backgroundtwo = {'normal': '#A8A878',
+        'dragon' : '#7038F8',
+        'water' : '#6890f0',
+        'fire' : '#F08030',
+        'ground' : '#B8A038',
+        'rock' : '#E0C068',
+        'electric' : '#F8D030',
+        'fairy': '#EE99AC',
+        'fighting': '#D67873',
+        'flying' : '#A8A878',
+        'ghost' : '#705898',
+        'grass' : '#A7DB8D',
+        'ice' : '#BCE6E6',
+        'poison' : '#A040A0',
+        'psychic' : '#F85888',
+        'steel' : '#D1D1E0',
+        'dark' : '#705848',
+        'bug' : '#C6D16E',
+        }
+
+        for key, value in backgroundtwo.items():
+            if self.typestwo in key:
+                return value
 
 
